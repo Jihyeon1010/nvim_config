d192039678f7a77a9897ef32604ae1ee0c23b615
Jihyeon Park <146726231+Jihyeon1010@users.noreply.github.com>
2025-04-11 06:01:05 +0900

    Add files via upload
    add files

---
+++ lua/community.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+}
---
+++ lua/plugins/astrocore.lua
@@ -0,0 +1,56 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    features = {
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    filetypes = {
+      extension = {
+        foo = "fooscript",
+      },
+      filename = {
+        [".foorc"] = "fooscript",
+      },
+      pattern = {
+        [".*/etc/foo/.*"] = "fooscript",
+      },
+    },
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = false,
+        number = true,
+        spell = false,
+        signcolumn = "yes",
+        wrap = false,
+      },
+      g = {},
+    },
+    mappings = {
+      n = {
+        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bd"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Close buffer from tabline",
+        },
+      },
+    },
+  },
+}
